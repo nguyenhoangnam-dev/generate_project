fdaadc92d00fb7edeecc3d384cdfdbcca39abfeb
NHN <congviec18120062@gmail.com>
2020-01-10 21:44:16 +0700

    Create folder and add file config

--- bin/index.js
+++ bin/index.js
@@ -15,41 +15,90 @@ const cssPreprocessor = ["none", "sass", "scss", "postcss", "stylus", "less"];
 const jsPreprocessor = ["none", "typescript", "coffeescript", "livescript"];
 
 var args = require("minimist")(process.argv.slice(2), {
-  boolean: ["help"],
-  string: ["name"]
+  boolean: ["help", "version"],
+  alias: { h: "help", v: "version" }
 });
 
 if (args.help) {
   help();
+} else if (args.version) {
+  // ncp.limit = 16;
 
-  rl.question(
-    "HTML preprocessor: ( " + htmlPreprocessor.join("| ") + ")",
-    function(name) {
-      rl.question("Where do you live ? ", function(country) {
-        fs.writeFile("newfile.txt", "abc", function(err) {
-          if (err) {
-            console.error(err.toString());
-          }
-          console.log("File is created successfully.");
-        });
-        rl.close();
-      });
-    }
-  );
-} else if (args.name) {
-  ncp.limit = 16;
-
-  ncp("F:\\2020\\gulp_scss", process.cwd() + "\\" + args.name, function error(
-    err
-  ) {
-    if (err) {
-      console.error(err.toString());
-    }
-    console.log("done!");
-  });
+  // ncp("F:\\2020\\gulp_scss", process.cwd() + "\\" + args.name, function error(
+  //   err
+  // ) {
+  //   if (err) {
+  //     console.error(err.toString());
+  //   }
+  //   console.log("done!");
+  // });
+  console.log("v.1.0.0");
 
   // console.log(__dirname + "\\" + args.name);
+} else if (args._[0] == "init") {
+  let overwrite = false;
+  let dir = process.cwd();
+  if (args._[1]) {
+    if (!fs.existsSync(dir + "\\" + args._[1])) {
+      dir = dir + "\\" + args._[1];
+    } else {
+      overwrite = true;
+      console.error("This file name is exist !");
+    }
+    if (!overwrite) {
+      fs.mkdirSync(dir);
+    }
+  }
+
+  if (!overwrite) {
+    rl.question(
+      "HTML preprocessor ( " + htmlPreprocessor.join(" | ") + " ): ",
+      function(html) {
+        if (!htmlPreprocessor.includes(html)) {
+          console.error("Invalid html preprocessor type file. ");
+          rl.close();
+        } else {
+          rl.question(
+            "CSS preprocessor ( " + cssPreprocessor.join(" | ") + " ): ",
+            function(css) {
+              if (!cssPreprocessor.includes(css)) {
+                console.error("Invalid css preprocessor type file. ");
+                rl.close();
+              } else {
+                rl.question(
+                  "JS preprocessor ( " + jsPreprocessor.join(" | ") + " ): ",
+                  function(js) {
+                    if (!jsPreprocessor.includes(js)) {
+                      console.error("Invalid js preprocessor type file. ");
+                      rl.close();
+                    } else {
+                      let configText = `${html} \n` + `${css} \n` + `${js}`;
+                      fs.writeFile(
+                        dir + "\\" + "generateConfig.txt",
+                        configText,
+                        function(err) {
+                          if (err) {
+                            console.error(err.toString());
+                          }
+                          console.log("File is created successfully.");
+                        }
+                      );
+                      rl.close();
+                    }
+                  }
+                );
+              }
+            }
+          );
+        }
+      }
+    );
+  }
+} else {
+  rl.close();
 }
+
+// *********************************************
 
 /**
  * Print help to remind command line able to use
