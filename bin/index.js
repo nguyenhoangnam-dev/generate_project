ffc6f9a775f9b8f0f6bea4f1b83b368a23116cc0
NHN <congviec18120062@gmail.com>
2020-01-15 19:30:58 +0700

    Add check error when create folder and file

--- bin/index.js
+++ bin/index.js
@@ -79,9 +79,7 @@ if (args.help) {
     // Check if option is exist
     if (!htmlPreprocessor.includes(html)) {
       // Show error
-      console.error(
-        chalk.red(emoji.get("x"), " Invalid html preprocessor type file. ")
-      );
+      showError("Invalid html preprocessor type file.", true);
     } else {
       // Store css preprocessor option if html option existed
       var css = prompt(
@@ -91,9 +89,7 @@ if (args.help) {
       // Check if option is existed
       if (!cssPreprocessor.includes(css)) {
         // Show error
-        console.error(
-          chalk.red(emoji.get("x"), " Invalid css preprocessor type file. ")
-        );
+        showError("Invalid css preprocessor type file.", true);
       } else {
         // Store js preprocessor option if css option in existed
         var js = prompt(
@@ -103,9 +99,11 @@ if (args.help) {
         // Check if option is existed
         if (!jsPreprocessor.includes(js)) {
           // show error
-          console.error(
-            chalk.red(emoji.get("x"), " Invalid js preprocessor type file. ")
-          );
+          // console.error(
+          //   chalk.red(emoji.get("x"), " Invalid js preprocessor type file. ")
+
+          // );
+          showError("Invalid js preprocessor type file", true);
         } else {
           // Content of config file
           let configText = `${html} \n` + `${css} \n` + `${js}`;
@@ -116,7 +114,8 @@ if (args.help) {
           ) {
             // Show error
             if (err) {
-              console.error(chalk.red(emoji.get("x"), err.toString()));
+              // console.error(chalk.red(emoji.get("x"), err.toString()));
+              showError(err.toString(), true);
             }
           });
         }
@@ -144,33 +143,46 @@ if (args.help) {
         makeSrc(dataOption, dir);
       } else {
         // Show error
-        console.error(chalk.red(emoji.get("x"), err.toString()));
+        showError(err.toString(), true);
       }
     });
   }
 } else if (args.tree) {
+  // Show tree of file
   showTree();
 } else {
-  showError(chalk.red(emoji.get("x") + "Can not find this command "), true);
+  showError("Can not find this command ", true);
+  // showError(chalk.red(emoji.get("x") + "Can not find this command "), true);
 }
 
 // *********************************************
 
+/**
+ * Show error and help if we need
+ * @param {string} mess This is error message we want to show
+ * @param {boolean} showHelp This check if we need to show help
+ */
 function showError(mess, showHelp = false) {
-  console.error(mess);
+  // Show error
+  console.error(chalk.red(emoji.get("x") + mess));
   if (showHelp) {
+    // Show help screen
     help();
   }
 }
 
+/**
+ * Show directory tree using gensetup
+ */
 function showTree() {
+  // Show directory of
   let dir1 = process.cwd();
   if (fs.existsSync(dir + "\\" + "genproject.json")) {
     let configData = fs.readFileSync(dir + "\\" + "genproject.json");
     let obj = JSON.parse(configData);
     console.log(treeify.asTree(obj, true, false));
   } else {
-    console.log(chalk.red("It missed gensetup.json"));
+    showError("It missed gensetup.json", true);
   }
 }
 
@@ -181,61 +193,7 @@ function showTree() {
  */
 function makeSrc(data, dir) {
   let objTree = {};
-
-  // TODO: Create .gitignore file to ignore git add node_modules in future
-  fs.writeFile(dir + "\\" + ".gitignore", "node_modules", function(err) {
-    if (err) {
-      //Show error
-      console.error(chalk.red(emoji.get("x"), err.toString()));
-    }
-    console.log(
-      chalk.green(
-        emoji.get("heavy_check_mark"),
-        " File .gitignore is created successfully."
-      )
-    );
-  });
-
-  // TODO: Create README.md file to show in github in future
-  fs.writeFile(dir + "\\" + "README.md", "", function(err) {
-    if (err) {
-      //Show error if can not create file
-      console.error(chalk.red(emoji.get("x"), err.toString()));
-    }
-    console.log(
-      chalk.green(
-        emoji.get("heavy_check_mark"),
-        " File README.md is created successfully."
-      )
-    );
-  });
-
-  // TODO: Create gulpfile.js file to show in github in future
-  fs.writeFile(dir + "\\" + "gulpfile.js", "", function(err) {
-    if (err) {
-      //Show error if can not create file
-      console.error(chalk.red(emoji.get("x"), err.toString()));
-    }
-    console.log(
-      chalk.green(
-        emoji.get("heavy_check_mark"),
-        " File gulpfile.js is created successfully."
-      )
-    );
-  });
-
-  // TODO: Create LICENSE file to show license of open source project in github in future
-  fs.writeFile(dir + "\\" + "LICENSE", "", function(err) {
-    if (err) {
-      console.error(chalk.red(emoji.get("x"), err.toString()));
-    }
-    console.log(
-      chalk.green(
-        emoji.get("heavy_check_mark"),
-        " File LICENSE is created successfully."
-      )
-    );
-  });
+  let checkError = false;
 
   // Store directory of src folder
   let dirsrc = path.join(dir, "src");
@@ -243,59 +201,142 @@ function makeSrc(data, dir) {
   // Create source folder
   fs.mkdirSync(dirsrc);
   objTree["src"] = {};
+
+  // TODO: Create .gitignore file to ignore git add node_modules in future
+  if (!checkError) {
+    fs.writeFile(dir + "\\" + ".gitignore", "node_modules", function(err) {
+      if (err) {
+        //Show error
+        showError(err.toString(), true);
+        checkError = true;
+      }
+      console.log(
+        chalk.green(
+          emoji.get("heavy_check_mark"),
+          " File .gitignore is created successfully."
+        )
+      );
+    });
+  }
+
+  // TODO: Create README.md file to show in github in future
+  if (!checkError) {
+    fs.writeFile(dir + "\\" + "README.md", "", function(err) {
+      if (err) {
+        //Show error if can not create file
+        showError(err.toString(), true);
+        checkError = true;
+      }
+      console.log(
+        chalk.green(
+          emoji.get("heavy_check_mark"),
+          " File README.md is created successfully."
+        )
+      );
+    });
+  }
+
+  // TODO: Create gulpfile.js file to show in github in future
+  if (!checkError) {
+    fs.writeFile(dir + "\\" + "gulpfile.js", "", function(err) {
+      if (err) {
+        //Show error if can not create file
+        showError(err.toString(), true);
+        checkError = true;
+      }
+      console.log(
+        chalk.green(
+          emoji.get("heavy_check_mark"),
+          " File gulpfile.js is created successfully."
+        )
+      );
+    });
+  }
+
+  // TODO: Create LICENSE file to show license of open source project in github in future
+  if (!checkError) {
+    fs.writeFile(dir + "\\" + "LICENSE", "", function(err) {
+      if (err) {
+        showError(err.toString(), true);
+        checkError = true;
+      }
+      console.log(
+        chalk.green(
+          emoji.get("heavy_check_mark"),
+          " File LICENSE is created successfully."
+        )
+      );
+    });
+  }
+
   // Default folder and file
   // Create css folder default
-  fs.mkdirSync(dirsrc + "\\" + "css");
-  objTree["src"]["css"] = {};
+  if (!checkError) {
+    fs.mkdirSync(dirsrc + "\\" + "css");
+    objTree["src"]["css"] = {};
+  }
+
   // Create main.css file in css folder default
-  fs.writeFile(dirsrc + "\\" + "css" + "\\" + "main.css", "", function(err) {
-    // Show error
-    if (err) {
-      console.error(chalk.red(emoji.get("x"), err.toString()));
-    }
-    console.log(
-      chalk.green(
-        emoji.get("heavy_check_mark"),
-        " File main.css is created successfully."
-      )
-    );
-    objTree["src"]["css"]["main.css"] = null;
-  });
+  if (!checkError) {
+    fs.writeFile(dirsrc + "\\" + "css" + "\\" + "main.css", "", function(err) {
+      // Show error
+      if (err) {
+        showError(err.toString(), true);
+        checkError = true;
+      }
+      console.log(
+        chalk.green(
+          emoji.get("heavy_check_mark"),
+          " File main.css is created successfully."
+        )
+      );
+      objTree["src"]["css"]["main.css"] = null;
+    });
+  }
 
   // Create js folder default
-  fs.mkdirSync(dirsrc + "\\" + "js");
-  objTree["src"]["js"] = {};
+  if (!checkError) {
+    fs.mkdirSync(dirsrc + "\\" + "js");
+    objTree["src"]["js"] = {};
+  }
+
   // Create index.js file in js folder default
-  fs.writeFile(dirsrc + "\\" + "js" + "\\" + "index.js", "", function(err) {
-    // Show error
-    if (err) {
-      console.error(chalk.red(emoji.get("x"), err.toString()));
-    }
-    console.log(
-      chalk.green(
-        emoji.get("heavy_check_mark"),
-        " File index.js is created successfully."
-      )
-    );
-    objTree["src"]["js"]["index.js"] = null;
-  });
+  if (!checkError) {
+    fs.writeFile(dirsrc + "\\" + "js" + "\\" + "index.js", "", function(err) {
+      // Show error
+      if (err) {
+        showError(err.toString(), true);
+        checkError = true;
+      }
+      console.log(
+        chalk.green(
+          emoji.get("heavy_check_mark"),
+          " File index.js is created successfully."
+        )
+      );
+      objTree["src"]["js"]["index.js"] = null;
+    });
+  }
 
   // Create index.html file default
-  fs.writeFile(dirsrc + "\\" + "index.html", "", function(err) {
-    if (err) {
-      console.error(chalk.red(emoji.get("x"), err.toString()));
-    }
-    console.log(
-      chalk.green(
-        emoji.get("heavy_check_mark"),
-        " File index.html is created successfully."
-      )
-    );
-    objTree["src"]["index.html"] = null;
-  });
+  if (!checkError) {
+    fs.writeFile(dirsrc + "\\" + "index.html", "", function(err) {
+      if (err) {
+        showError(err.toString(), true);
+        checkError = true;
+      }
+      console.log(
+        chalk.green(
+          emoji.get("heavy_check_mark"),
+          " File index.html is created successfully."
+        )
+      );
+      objTree["src"]["index.html"] = null;
+    });
+  }
 
   // Check if user contain html preprocessor
-  if (data[0] != "none") {
+  if (data[0] != "none" && !checkError) {
     // Create folder with data type
     fs.mkdirSync(dirsrc + "\\" + data[0]);
     objTree["src"][data[0]] = {};
@@ -306,7 +347,8 @@ function makeSrc(data, dir) {
       function(err) {
         if (err) {
           // Show error
-          console.error(chalk.red(emoji.get("x"), err.toString()));
+          showError(err.toString(), true);
+          checkError = true;
         }
         console.log(
           chalk.green(
@@ -320,7 +362,7 @@ function makeSrc(data, dir) {
   }
 
   // Check if user contain css preprocessor
-  if (data[1] != "none") {
+  if (data[1] != "none" && !checkError) {
     // Create folder with data type
     fs.mkdirSync(dirsrc + "\\" + data[1]);
     objTree["src"][data[1]] = {};
@@ -331,7 +373,8 @@ function makeSrc(data, dir) {
       function(err) {
         if (err) {
           // Show error
-          console.error(chalk.red(emoji.get("x"), err.toString()));
+          showError(err.toString(), true);
+          checkError = true;
         }
         console.log(
           chalk.green(
@@ -345,7 +388,7 @@ function makeSrc(data, dir) {
   }
 
   // Check if user contain js preprocessor
-  if (data[2] != "none") {
+  if (data[2] != "none" && !checkError) {
     // Check if user use typescript
     if (data[2] == "typescript") {
       // Create ts folder instead of typescript
@@ -355,7 +398,8 @@ function makeSrc(data, dir) {
       fs.writeFile(dirsrc + "\\" + "ts" + "\\" + `index.ts`, "", function(err) {
         if (err) {
           // Show error
-          console.error(chalk.red(emoji.get("x"), err.toString()));
+          showError(err.toString(), true);
+          checkError = true;
         }
         console.log(
           chalk.green(
@@ -376,7 +420,7 @@ function makeSrc(data, dir) {
         function(err) {
           if (err) {
             // Show error
-            console.error(chalk.red(emoji.get("x"), err.toString()));
+            showError(err.toString(), true);
           }
           console.log(
             chalk.green(
@@ -391,19 +435,29 @@ function makeSrc(data, dir) {
   }
 
   // TODO: Create font folder for store font in future
-  fs.mkdirSync(dirsrc + "\\" + "font");
-  objTree["src"]["font"] = null;
-  // TODO: Create img folder for store img in future
-  fs.mkdirSync(dirsrc + "\\" + "img");
-  objTree["src"]["img"] = null;
-  // TODO: Create lib folder for store lib in future
-  fs.mkdirSync(dirsrc + "\\" + "lib");
-  objTree["src"]["lib"] = null;
+  if (!checkError) {
+    fs.mkdirSync(dirsrc + "\\" + "font");
+    objTree["src"]["font"] = null;
+  }
 
-  objTree[".gitignore"] = null;
-  objTree["gulpfile.js"] = null;
-  objTree["README.md"] = null;
-  objTree["LICENSE"] = null;
+  // TODO: Create img folder for store img in future
+  if (!checkError) {
+    fs.mkdirSync(dirsrc + "\\" + "img");
+    objTree["src"]["img"] = null;
+  }
+
+  // TODO: Create lib folder for store lib in future
+  if (!checkError) {
+    fs.mkdirSync(dirsrc + "\\" + "lib");
+    objTree["src"]["lib"] = null;
+  }
+
+  if (!checkError) {
+    objTree[".gitignore"] = null;
+    objTree["gulpfile.js"] = null;
+    objTree["README.md"] = null;
+    objTree["LICENSE"] = null;
+  }
 
   // Delete config file after create success folder
   fs.unlinkSync(dir + "\\" + "generateConfig.txt");
@@ -411,17 +465,28 @@ function makeSrc(data, dir) {
     function(objTree) {
       var json = JSON.stringify(objTree);
       // TODO: Create LICENSE file to show license of open source project in github in future
-      fs.writeFile(dir + "\\" + "genproject.json", json, function(err) {
-        if (err) {
-          console.error(chalk.red(emoji.get("x"), err.toString()));
-        }
-        console.log(
-          chalk.green(
-            emoji.get("heavy_check_mark"),
-            " File genproject.json is created successfully."
-          )
-        );
-      });
+      if (!checkError) {
+        fs.writeFile(dir + "\\" + "genproject.json", json, function(err) {
+          if (err) {
+            showError(err.toString(), true);
+            checkError = true;
+          }
+          console.log(
+            chalk.green(
+              emoji.get("heavy_check_mark"),
+              " File genproject.json is created successfully."
+            )
+          );
+          console.log();
+          console.log();
+          let dirArr = dir.split("\\");
+          let folderName = dirArr[dirArr.length - 1];
+          console.log(chalk.green("Let go to directory:    cd " + folderName));
+          console.log(chalk.green("Show folder and file:   gensetup -t"));
+        });
+      } else {
+        showError("Fail to create folder ", true);
+      }
     },
     1000,
     objTree
